3029b6a5b77a6ccfe8ee45a239cce505579eb0a0
harshaks23 <harshaksabbur@gmail.com>
2018-05-28 14:16:06 -0500

    bst coded

--- src/CoreDS/BST.java
+++ src/CoreDS/BST.java
@@ -21,17 +21,16 @@ public class Node
     }
     public Node insertKey(Node node,int key)
     {
-        if ( node==null )
-        {
+        if(node==null){
             node= new Node(key);
-            return node;
-        }  else
+            return node;}
+        if (key<=node.key)
         {
-            if (key<=node.key)
-                node.left=insertKey(node.left,key);
-            else
-                node.right=insertKey(node.right,key);
+            node.left=insertKey(node.left,key);
         }
+        else
+            node.right=insertKey(node.right,key);
+
         return node;
     }
 public void delete(int no)
@@ -39,42 +38,62 @@ public void delete(int no)
     if (root==null) System.out.println("empty tree");
     root= delete_node(root,no);
 
+
 }
 public Node delete_node(Node node,int key) {
-    if (node == null)
-    {
-              return node;
-}
-    if (key<node.key) node.left=delete_node(node.left,key);
-    else if (key>node.key) node.right= delete_node(node.right,key);
-    else
-        {
-            if (node.left==null)
-                return node.right;
-            else if (node.right==null)
-                return node.left;
-            else {
-               node.key= find_min_successor(node.right);
-               node.right=delete_node(node.right,key);
-            }
-        }
-        return root;
-}
 
+    if (node==null)
+        return node;
+    if (key<root.key)
+        node.left=delete_node(node.left,key);
+    else if (key>root.key)
+        node.right=delete_node(node.right,key);
+    else
+    {
+        if (node.left==null)
+            return node.right;
+        if (node.right==null)
+            return node.left;
+        node.key =find_min_successor(node.right);
+        node.right=delete_node(node.right,node.key);
+    }
+return node;
+}
 public int find_min_successor(Node node)
 {
     int min=node.key;
-    while (node.left!=null)
+    while (node!=null)
     {
-        node=node.left;
         min=node.key;
+        node=node.left;
     }
-
     return min;
+}
+public  void inorder()
+{
+    inorder_(root);
+}
+public void inorder_(Node node)
+{
+    if (node==null)
+        return;
+    inorder_(node.left);
+    System.out.print(node.key+" ");
+    inorder_(node.right);
+
 
 }
     public static void main(String[] args) {
         BST bst = new BST();
         bst.insert(1);
+        bst.insert(3);
+        bst.insert(2);
+        bst.insert(4);
+        bst.insert(5);
+        bst.insert(6);
+        bst.inorder();
+bst.delete(1);
+        System.out.println("-----------");
+bst.inorder();
     }
 }
