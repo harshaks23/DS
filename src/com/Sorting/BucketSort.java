d6ea3f2a0beb1593f93331b64f15c355aca56dde
harshaks23 <harshaksabbur@gmail.com>
2018-06-02 23:21:07 -0500

    bucketsort

---
+++ src/com/Sorting/BucketSort.java
@@ -0,0 +1,60 @@
+package com.Sorting;
+
+public class BucketSort  {
+public int max(int[] arr)
+{
+    int max=-1;
+    for (int i:arr)
+    {
+        if (arr[i]>max)
+            max=i;
+    }
+    return max;
+}
+
+
+    public int[] sort(int[] arr)
+    {
+       int max= max(arr);
+       int[] bucket_array= new int[max+1];
+       for (int i=0;i<bucket_array.length;i++)
+       {
+           bucket_array[i]=0;
+       }
+        for (int i=0;i<arr.length;i++)
+        {
+            bucket_array[arr[i]]++;
+        }
+int start=0;
+        for ( int i=0;i<bucket_array.length;i++)
+        {
+            for (int j=0;j<bucket_array[i];j++)
+            {
+                arr[start++]=i;
+            }
+
+        }
+
+
+
+return arr;
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr= new int[]{8,2,4,5,1,2,7,6,3,2};
+        BucketSort bucketSort = new BucketSort();
+        arr=bucketSort.sort(arr);
+        for (int i:arr
+             ) {
+
+            System.out.println(i);
+        }
+
+    }
+
+}
