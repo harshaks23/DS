9f35684efa5ff7db7ef30f41ca0ba86db61730fc
harshaks23 <harshaksabbur@gmail.com>
2018-06-03 20:37:51 -0500

    tortize has cycle

---
+++ src/LinkedList/tortize_hasCycle.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class tortize_hasCycle {
+    public boolean hasCycle(ListNode head)
+    {
+
+        if(head==null || head.next==null) return false;
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null)
+        {
+
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+                return true;
+
+
+
+        }
+
+        return false;
+
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
