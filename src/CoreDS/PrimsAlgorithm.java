8993d89c7bbb49ec503e5cbca648b374eed0220b
harshaks23 <harshaksabbur@gmail.com>
2018-06-04 22:08:29 -0500

    prims

---
+++ src/CoreDS/PrimsAlgorithm.java
@@ -0,0 +1,35 @@
+package CoreDS;
+
+public class PrimsAlgorithm {
+int V=5;
+    public  void  generate(int[] graph)
+    {
+        int[] keys = new int[V];
+        int[] parent = new int[V];
+        boolean[] check = new boolean[V];
+
+        for (int i=0;i<V;i++)
+        {
+            
+        }
+
+        for (int i=0;i<V-1;i++)
+        {
+
+
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int graph[][] = new int[][] {{0, 2, 0, 6, 0},
+                {2, 0, 3, 8, 5},
+                {0, 3, 0, 0, 7},
+                {6, 8, 0, 0, 9},
+                {0, 5, 7, 9, 0},
+        };
+    }
+}
