4b4a3e8bf7a03127948e3d9bff0bde1190393481
harshaks23 <harshaksabbur@gmail.com>
2018-06-09 18:26:56 -0500

    plusone

--- src/LinkedList/starPLUSoneLinked.java
+++ src/LinkedList/starPLUSoneLinked.java
@@ -1,4 +1,18 @@
 package LinkedList;
+/*
+Given a non-negative integer represented as non-empty a singly linked list of digits, plus one to the integer.
+
+        You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+        The digits are stored such that the most significant digit is at the head of the list.
+
+        Example:
+        Input:
+        1->2->3
+
+        Output:
+        1->2->4
+*/
 
 public class starPLUSoneLinked {
 
@@ -8,7 +22,7 @@ public class starPLUSoneLinked {
       ListNode next;
       ListNode(int x) { val = x; }
   }
- 
+
     class Solution {
         public ListNode plusOne(ListNode head) {
 
