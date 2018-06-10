d76341fedebe743f15ad2bbbe45e4b40dda1cd79
harshaks23 <harshaksabbur@gmail.com>
2018-06-09 19:25:51 -0500

    add two no wih o(1) and n space

---
+++ src/LinkedList/addtwono2.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+import java.util.Stack;
+//this is n space ,we can do it in with 0(1) with reverse and traversing
+public class addtwono2 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+
+        return list.val == 0 ? list.next : list;
+    }
+}
