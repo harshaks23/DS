cc9f504d84c91df741ef71f1d602664abfa807f0
harshaks23 <harshaksabbur@gmail.com>
2018-06-13 06:13:57 -0500

    num of island

---
+++ src/BinaryTree/numofIsland.java
@@ -0,0 +1,43 @@
+package BinaryTree;
+
+public class numofIsland {
+
+    class Solution {
+        public int numIslands(char[][] grid) {
+
+
+            int count=0;
+            for(int i=0;i<grid.length;i++)
+            {
+                for(int j=0;j<grid[i].length;j++)
+                {
+
+                    if(grid[i][j]=='1')
+                        helper(grid,i,j);
+                    count++;
+                }
+            }
+
+            return count;
+
+        }
+
+        public void helper(char[][] grid,int  i,int j)
+        {
+            if(i>=0 && i<grid.length && j>= 0 && j<grid[i].length && grid[i][j]=='1')
+            {
+
+                grid[i][j]='2';
+                helper(grid,i-1,j);
+
+                helper(grid,i+1,j);
+
+                helper(grid,i,j-1);
+
+                helper(grid,i,j+1);
+            }
+            return ;
+        }
+    }
+
+}
