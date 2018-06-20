585d4a53ccce9ee1b09a33dce7c6254331aec3c1
harshaks23 <harshaksabbur@gmail.com>
2018-06-19 21:19:41 -0500

    find max occurances

--- src/BinaryTree/FindMaxallOccurancesinBST.java
+++ src/BinaryTree/FindMaxallOccurancesinBST.java
@@ -1,4 +1,64 @@
 package BinaryTree;
 
+import java.util.*;
+
 public class FindMaxallOccurancesinBST {
+
+    class Solution {
+
+        List<Integer> list= new ArrayList<>();
+        int max=0;
+        int count=0;
+        TreeNode prev;
+        public int[] findMode(TreeNode root)
+        {
+            if(root==null) return  new int[0];
+
+            recur(root);
+            int[] ret= new int[list.size()];
+            for(int i=0;i<ret.length;i++)
+            {
+                ret[i]=list.get(i);
+            }
+
+            return ret;
+
+        }
+
+        void  recur(TreeNode node)
+        {
+            if(node==null)
+                return;
+            recur(node.left);
+
+            if(prev!=null && prev.val==node.val)
+            {
+                count++;
+
+            }else
+                count=1;
+
+            if(count>max)
+            {
+                max=count;
+                list.clear();
+                list.add(node.val);
+            }
+            else
+            if(count==max)
+                list.add(node.val);
+
+            prev=node;
+            recur(node.right);
+        }
+    }
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 }
