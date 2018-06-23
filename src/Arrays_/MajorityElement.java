5ed537df99719c819e8dfa839d54c6384e757aeb
harshaks23 <harshaksabbur@gmail.com>
2018-06-23 15:53:55 -0500

    majority element2

--- src/Arrays_/MajorityElement.java
+++ src/Arrays_/MajorityElement.java
@@ -6,9 +6,9 @@ public class MajorityElement {
         public int majorityElement(int[] nums)
         {
 
-            int count=1;
+            int count=0;
             int element=nums[0];
-            for(int i=1;i<nums.length;i++)
+            for(int i=0;i<nums.length;i++)
             {
                 if(nums[i]!=element)
                 {
---
+++ src/Arrays_/ProductOfArrayExcceptItself.java
@@ -0,0 +1,27 @@
+package Arrays_;
+
+public class ProductOfArrayExcceptItself {
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int prev=nums[0];
+            //as we cant intiailaize first element in first pass we set it to 1
+
+            int[] res= new int[nums.length];
+            res[0]=1;
+            for(int i=1;i<res.length;i++)
+            {
+                res[i]=res[i-1]*prev;
+                prev=nums[i];
+            }
+
+            prev=nums[nums.length-1];
+            for(int i=res.length-2;i>=0;i--)
+            {
+                res[i]=res[i]*prev;
+                prev*=nums[i];
+            }
+
+            return res;
+        }
+    }
+}
