3525d2cd8377eb7a5f4bf2b79a541e22cbfe6311
harshaks23 <harshaksabbur@gmail.com>
2018-06-25 13:34:24 -0500

    next permuation

--- src/Stack_/nextElement2.java
+++ src/Stack_/nextElement2.java
@@ -3,6 +3,10 @@ package Stack_;
 import java.util.Arrays;
 import java.util.Stack;
 
+/**
+ * Given a circular array (the next element of the last element is the first element of the array), print the Next Greater Number for every element. The Next Greater Number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, output -1 for this number.
+ * **/
+
 public class nextElement2 {
 
    // smaller length code and explanation
