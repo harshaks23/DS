5de9c04a280617a9a66b53207c525dbbfeaf875d
harshaks23 <harshaksabbur@gmail.com>
2018-06-26 14:35:39 -0500

    Two sum2 sorted

---
+++ src/TwoPointers/TwoSum2.java
@@ -0,0 +1,20 @@
+package TwoPointers;
+
+public class TwoSum2 {
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int p1=0,p2=numbers.length-1;
+
+            while(p1<p2)
+            {
+                int sum=numbers[p1] +numbers[p2];
+                if(sum==target) return new int[]{p1+1,p2+1};
+                if(sum<target) p1++;
+                else
+                    p2--;
+            }
+            return new int[]{-1,-1};
+
+        }
+    }
+}
